92198c01d9c6e8ea9d8d1a4a475c47ffe872a970
David Montesdeoca <backpackerhh@gmail.com>
2019-12-15 20:50:21 +0100

    Add function to fetch repos from GitHub
    That function takes in a language and, using the GitHub API, will
    fetch the most popular repos for that language.

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import serialize from "serialize-javascript"
 
 import App from '../shared/App';
+import { fetchPopularRepos } from '../shared/api'
 
 const app = express();
 
@@ -12,24 +13,26 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("*", (req, res, next) => {
-  const name = "David"
-  const markup = renderToString(<App data={name} />);
+  fetchPopularRepos()
+    .then((data) => {
+      const markup = renderToString(<App data={data} />);
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>SSR with RR</title>
-        <script src="/bundle.js" defer></script>
-        <script>window.__INITIAL_DATA__ = ${serialize(name)}</script>
-      </head>
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>SSR with RR</title>
+          <script src="/bundle.js" defer></script>
+          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+        </head>
 
-      <body>
-        <div id="app">${markup}</div>
-      </body>
-    </html>
-  `
-  );
+        <body>
+          <div id="app">${markup}</div>
+        </body>
+      </html>
+    `
+    );
+  });
 });
 
 app.listen(3000, () => {
